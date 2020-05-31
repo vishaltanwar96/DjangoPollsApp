e4a7d7912b4251b6a616484cf326f9d3137b4153
Vishal Tanwar <vshal@authbridge.com>
2020-05-31 23:38:33 +0530

    changed name parameter value in path function to snake_case

--- Polls/polls/urls.py
+++ Polls/polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns: list = [
     path('', views.index, name='index'),
-    path('detail/<int:question_id>/', views.detail, name='Question detail'),
-    path('result/<int:question_id>/', views.result, name='Question result'),
+    path('detail/<int:question_id>/', views.detail, name='question_detail'),
+    path('result/<int:question_id>/', views.result, name='question_result'),
     path('vote/<int:question_id>/', views.index, name='index')
 ]
