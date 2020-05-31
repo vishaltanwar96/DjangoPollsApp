01ad6c6060e40809e68e8e655c0f1a84980fe1cf
Vishal Tanwar <vshal@authbridge.com>
2020-06-01 00:13:51 +0530

    changed name value index to vote for vote url

--- Polls/polls/urls.py
+++ Polls/polls/urls.py
@@ -5,5 +5,5 @@ urlpatterns: list = [
     path('', views.index, name='index'),
     path('detail/<int:question_id>/', views.detail, name='question_detail'),
     path('result/<int:question_id>/', views.result, name='question_result'),
-    path('vote/<int:question_id>/', views.index, name='index')
+    path('vote/<int:question_id>/', views.index, name='vote')
 ]
